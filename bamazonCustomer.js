cb38e052f1ca2a0105255df017827b3b08c85cdd
Nate <nathanperfetti@gmail.com>
2018-06-06 18:00:32 -0500

    Updated UI for customer script

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,104 +7,109 @@ const connection = mysql.createConnection({
     host: "localhost",
     // Your port; if not 3306
     port: 8889,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "root",
     database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
+});//End connection definition
+
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("Connected to bamazon DB on port 8889!!\n");
+    //console.log("Connected to bamazon DB on port 8889!!\n");
+    console.log('|||||| ------------------ Welcome to Bamazon! ------------------||||||| \n');
     console.log('|||||| ------------------ Check Out All our Great Products ------------------||||||| \n');
     queryAll();
 });
 
 //Start queryAll()
-function queryAll(){
-    
+function queryAll() {
+
     connection.query('SELECT * FROM products', function (err, res) {
         //Table creation w/out console.table from cTable Package: var results = "Item Id : " + res.item_id + "|" + . . . .
         console.table(res);
         //console.log(res);
         inquireCustomer();
-    } );
+    });
 
-  };//End queryAll();
+}; //End queryAll();
 
 //Start inquireCustomer();
-function inquireCustomer(){
+function inquireCustomer() {
     inquirer.prompt([{
         //Question 1:
         name: 'userProdId',
         type: 'input',
         message: 'What is the Id of the product you want to buy?'
-    },{
+    }, {
         //Question 2:
         name: 'userQty',
         type: 'input',
         message: 'How many of this product would you like to buy?'
     }]).then(
-        function(userInput){
-            console.log("Item Id Selected: " + userInput.userProdId);
-            console.log("Qty requested: " + userInput.userQty);
+        function (userInput) {
+            console.log("\n||------- Order Summary:");
+            console.log("\n||||------- Item Id Selected: " + userInput.userProdId);
+            console.log("\n||||------- Qty requested: " + userInput.userQty);
 
             checkInventory(userInput);
         }
-    )
-};//End inquireCustomer();
+    ); //End .prompt() callback fct
+}; //End inquireCustomer();
 
 //=============Start checkInventory()
-function checkInventory(orderJSON){
+function checkInventory(orderJSON) {
     var productId = orderJSON.userProdId;
     var qtyOrdered = orderJSON.userQty;
 
     connection.query(
-        "SELECT * FROM products WHERE ?",
-    {
-        item_id: productId
-    }, 
-    
-    function(err, res){
+        "SELECT * FROM products WHERE ?", 
+        {
+            item_id: productId
+        },
+        function (err, res) {
+            var productRow = res[0];
+            var availQty = productRow .stock_quantity;
+            var productOrdered = productRow .product_name;
+            var diffAvailOrd = availQty - qtyOrdered;
+            //console.log("New Inventory should be " + diffAvailOrd);
 
-        var availQty = res[0].stock_quantity;
-        var productOrdered = res[0].product_name;
-        var diffAvailOrd = availQty - qtyOrdered;
-        //console.log("New Inventory should be " + diffAvailOrd);
-        
-        //Check if order can be fulfilled 
-        if(diffAvailOrd >= 0){
-        console.log("We have " + availQty + " of these " + productOrdered + "s" + ", and you ordered " + qtyOrdered + ".");
+            //Check if order can be fulfilled 
+            if (diffAvailOrd >= 0) {
+                console.log("\n||------- We have " + availQty + " of these " + productOrdered + "s" + ", and you ordered " + qtyOrdered + ".");
 
-        //Execute database update given customer's order can be fulfilled
-        decrementInventory(diffAvailOrd, productId);
+                //Execute database update given customer's order can be fulfilled
+                decrementInventory(diffAvailOrd, productId);
 
-        } else {
-            console.log("Insufficient quantity!");
-            console.log("Our apologies, we do not have enough available quatity of these " + productOrdered + "s" + " to fulfill your order.");
-            console.log("Please place a different order.");
-            queryAll();
-        }
-    });//End SELECT connection.query()
-};//End checkInventory()===================|
+                //Print Receipt After SQL UPDATE
+                console.log("\n||------- Thanks for Shopping with Bamazon");
+                console.log("\n||------- Customer Receipt:");
+                console.table(productRow);
+
+            } else {
+                console.log("Insufficient quantity!");
+                console.log("Our apologies, we do not have enough available quantity of these " + productOrdered + "s" + " to fulfill your order.");
+                console.log("Please place a different order.");
+                queryAll();
+            }
+        }); //End SELECT connection.query()
+}; //End checkInventory()===================|
 
 //=============Start decrementInventory()
-function decrementInventory(newInvAmt, productId){
+function decrementInventory(newInvAmt, productId) {
     connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [{
+        "UPDATE products SET ? WHERE ?", [{
             stock_quantity: newInvAmt
-        },{
+        }, {
             item_id: productId
         }],
-        function(err,res){
-            console.log(res);
+        function (err, res) {
+            //console.log(res);
             //console.log(res.affectedRows + " products updated!\n");
-            console.log("Congratulations! Your order has been submitted!");
+            console.log("\n||------- Congratulations! Your order has been submitted!");
             queryAll();
-        }//End connection.query() callback fct
-    );//End UPDATE connection.query()
-};//End decrementInventory()===================|
+        } //End connection.query() callback fct
+    ); //End UPDATE connection.query()
+}; //End decrementInventory()===================|
